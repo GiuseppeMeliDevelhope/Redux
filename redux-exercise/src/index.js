f689f84894be40bec481e68b55397e877d719925
Giuseppe Meli <meligiuseppe92020@gmail.com>
2023-03-08 13:25:29 +0100

    exercise-3

--- redux-exercise/src/index.js
+++ redux-exercise/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import CounterReducer from './CounterSlice';
+import todosSlice from './todosSlice';
 import App from './App';
 
 const store = configureStore({
   reducer: {
     counter: CounterReducer,
+    todos: todosSlice
   },
 });
 
